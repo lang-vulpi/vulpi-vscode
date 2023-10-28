7fcaa9541dafe1618c2ff79c62abe697828d5dd0
Daniel de Sá <danidevpy@gmail.com>
2023-10-28 18:32:39 -0300

    feat: init lsp using lsp bin

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,59 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from 'vscode-languageclient/node';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+let client: LanguageClient;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vulpi" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vulpi.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Vulpiiiii!');
-	});
-
-	context.subscriptions.push(disposable);
+function hello() {
+  vscode.window.showInformationMessage('Hello World from Vulpi!');
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function initLSP() { // TODO: look for a better name
+  const serverPath = path.join("/home/danitw/development/vulpi/target/debug/vulpi-cli"); // Replace with your LSP binary name
+
+  const serverOptions: ServerOptions = {
+    run: { command: serverPath, transport: TransportKind.stdio },
+    debug: { command: serverPath, transport: TransportKind.stdio },
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: 'file', language: 'plaintext' }], // Specify your language identifier
+  };
+
+  client = new LanguageClient(
+    'vulpiLanguageServer', // Replace with your extension ID
+    'Vulpi Language Server', // Replace with your extension name
+    serverOptions,
+    clientOptions
+  );
+
+  // context.subscriptions.push(client.start());
+  client.start();
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('initializing Vulpi!');
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand('vulpi.helloWorld', hello);
+
+  context.subscriptions.push(disposable);
+
+  initLSP();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
